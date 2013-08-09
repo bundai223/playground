047813c0122b3bd522624e57b73f59dc27956ec9
Bundai223 <bundai223@gmail.com>
2013-08-09 23:05:30 +0900

    add cpp by clang

---
+++ lang/cpp/Cpp11/main.cpp
@@ -0,0 +1,14 @@
+//**********************************************************************
+//! @file   main.cpp
+//! @brief  describe
+//! @author d_nishimura
+//**********************************************************************
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World." << std::endl;
+
+    return 0;
+}
+
