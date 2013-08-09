f70f05a4e2badd170e93b0f991d2a4bcaf19cfe1
Bundai223 <bundai223@gmail.com>
2013-08-10 02:22:03 +0900

    add cpp11 test code

--- lang/cpp/Cpp11/main.cpp
+++ lang/cpp/Cpp11/main.cpp
@@ -4,11 +4,19 @@
 //! @author d_nishimura
 //**********************************************************************
 #include <iostream>
+#include <vector>
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello World." << std::endl;
+    std::vector<int> hoge;
+    hoge.push_back(1);
+    hoge.push_back(10);
+    hoge.push_back(5);
 
+    for(auto it = hoge.begin(); it != hoge.end(); ++it)
+    {
+        std::cout << "Hello. :" << *it << std::endl;
+    }
     return 0;
 }
 
