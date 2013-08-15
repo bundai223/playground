06fd962a567f002560ba865f5b40a3d896687ef3
bundai223 <bundai223@yahoo.co.jp>
2013-08-15 13:01:04 +0900

    add algorithm dir

---
+++ algorithm/cpp11/main.cpp
@@ -0,0 +1,16 @@
+//**********************************************************************
+//! @file   main.cpp
+//! @brief  describe
+//! @author d_nishimura
+//**********************************************************************
+#include <iostream>
+#include <vector>
+
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World." << std::endl;
+
+    return 0;
+}
+
---
+++ algorithm/cpp11/sort.cpp
@@ -0,0 +1,18 @@
+//**********************************************************************
+//! @file   sort.cpp
+//! @brief  describe
+//**********************************************************************
+#include "sort.h"
+
+
+namespace algorithm {
+
+    template <typename Itrator, typename CompFunc>
+    void quicksort(Iterator& begin, Iterator& end, CompFunc& compare)
+    {
+        (void)begin;
+        (void)end;
+        (void)compare;
+    }
+
+} // namespace algorithm
