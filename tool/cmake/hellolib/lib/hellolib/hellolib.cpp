6e3209ac34e2ca3ac77b8aa2324577e1a4e9f772
bundai223 <bundai223@yahoo.co.jp>
2013-08-19 13:02:24 +0900

    add cmake with personal lib test

---
+++ tool/cmake/hellolib/lib/hellolib/hellolib.cpp
@@ -0,0 +1,15 @@
+//**********************************************************************
+//! @file   main.cpp
+//! @brief  describe
+//**********************************************************************
+#include <iostream>
+#include "hellolib.h"
+
+namespace hellolib
+{
+    void hello(void)
+    {
+        std::cout << "hello cmake with library." << std::endl;
+    }
+} // namespace hellolib
+
---
+++ tool/cmake/hellolib/lib/hellolib/hellolib.h
@@ -0,0 +1,16 @@
+//**********************************************************************
+//! @file   main.cpp
+//! @brief  describe
+//**********************************************************************
+#ifndef HELLO_H_INCLUDED
+#define HELLO_H_INCLUDED
+
+
+namespace hellolib {
+
+    void hello();
+
+} // hellolib
+
+#endif // HELLO_H_INCLUDED
+
